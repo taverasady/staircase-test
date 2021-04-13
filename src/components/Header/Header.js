fa77030e74ff955cb3c3edd77559f4c81cdeba2b
Ady Taveras Courleaux <ady.taveras@solvex.com.do>
2021-04-13 09:31:08 -0400

    progress

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import { useStyles } from './styles.js'
-import logo from "../../assets/images/logo.png"
+import { useStyles } from './styles.js';
+import logo from "../../assets/images/logo.png";
 
 function Header() {
   const classes = useStyles();
     return (
       <AppBar className={classes.appBarColor} position="static">
       <Toolbar>
-        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-        </IconButton>
         <img src={logo} className={classes.logoSize} alt="staircase"/>
       </Toolbar>
     </AppBar>
